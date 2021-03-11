b0aca9cf7faef605f07ff02e5d33e999f8f49a83
MAriefW <wicaksonoma15d@student.unhas.ac.id>
2021-03-12 00:09:09 +0800

    Fix Ro Value & Declaration ppm variabel
    nilai Ro dapat diinput sendiri pada program tanpa edit di library, variabel ppm di deklarasikan di file NH3.h agar dapat digunakan pada file .ino

--- NH3.cpp
+++ NH3.cpp
@@ -1,13 +1,13 @@
 #include "NH3.h"
-#define RL 47  //value of Load Resistance in datasheet
-#define m -0.417 //Calculated Gradient 
-#define b 0.425 //Calculated intercept
-#define Ro 20 //Ro value
+#define RL 10  //The value of resistor RL is 10K
+#define m -0.417 //Calculated Slope 
+#define b 0.858 //Calculated intercept
 
-float VRL; //voltage drop across the MQ sensor
-float Rs; //sensor resistance at gas concentration
-float ratio; //define variable for ratio
-float ppm; //define NH3 value
+float VRL; //Voltage drop across the MQ sensor
+float Rs; //Sensor resistance at gas concentration
+float ratio; //Define variable for ratio Rs/Ro
+//float ppm; //Ammonia value
+float MaxRo = 0; //Sensor Resistance at air concentration
 
 NH3::NH3(byte pin) {
   this->pin = pin;
@@ -15,21 +15,21 @@ NH3::NH3(byte pin) {
 }
 
 void NH3::init() {
-  pinMode(pin, INPUT); 
+  pinMode(pin, INPUT);
 }
-
-void NH3::sensorValue() {
-  Serial.println(analogRead(pin)); //print sensor value
+void NH3::nilai_Ro(float Ro){
+	this->Ro = Ro;
 }
-
-void NH3::ppmValue() {
-  Rs = (5.0 / VRL - 1)*RL; //formula to get Rs value without load resistance applied
-  ratio = Rs / Ro; //formula to get ratio
+void NH3::nilai_ppm() {
+  VRL = analogRead(pin) * (5.0 / 1023.0); //Measure the voltage drop and convert to 0-5V
+  Rs = ((5.0 * RL) / VRL) - RL; //Formula to get Rs value
+  ratio = Rs / Ro; // find ratio Rs/Ro
   ppm = pow(10, ((log10(ratio) - b) / m)); //use formula to calculate ppm
-  Serial.println(ppm); //print ppm value
+  Serial.println(ppm);
 }
-
-void NH3::vrlValue() {
-VRL = analogRead(pin) * (5.0 / 1023.0); //Measure the voltage drop and convert to analog
-Serial.println(VRL); //print voltage drop value
+void NH3::nilai_vrl() {
+	Serial.println(VRL);
+}
+void NH3::nilai_sensor() {
+  Serial.println(analogRead(pin));
 }
